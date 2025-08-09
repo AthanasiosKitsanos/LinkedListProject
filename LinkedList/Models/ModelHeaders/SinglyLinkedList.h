183d9512b4b50fc5051caf7707a8c7fe1780fc31
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-08-09 17:46:50 +0300

    SinglyLinkedList header for declarations

--- LinkedList/Models/ModelHeaders/SinglyLinkedList.h
+++ LinkedList/Models/ModelHeaders/SinglyLinkedList.h
@@ -15,6 +15,11 @@ struct SinglyLinkedList
     void clear();
     void pop_front();
     void push_back(const T& value);
+    void insert_after(const size_t& target, const T& value);
+    void insert_before(const size_t& target, const T& value);
+    Node<T>* create_node(const T& element);
+    void replace_at(const size_t& index, const T& value);
+    int find(const T& value);
 };
 
 #include "SinglyLinkedList.tpp"
